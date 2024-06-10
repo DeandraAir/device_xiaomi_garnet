732e3eb4dad2141d487b4f74f1910cbebd2efbe1
Nanda77 <142644567+DeandraAir@users.noreply.github.com>
2024-06-10 14:12:39 +0700

    garnet: Add dolby to cloned apps exemption list
    Change-Id: I47ab06f7d5152c4c993359ac85d121e41c8df235
    Signed-off-by: ramshell688 <ramonzost68@gmail.com>

--- device.mk
+++ device.mk
@@ -154,6 +154,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libsuspend \
     charger_res_images
+    
+# Clone apps exemption
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sysconfig/preinstalled-packages-platform-xiaomi-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-xiaomi-product.xml
 
 # Configstore
 PRODUCT_PACKAGES += \
