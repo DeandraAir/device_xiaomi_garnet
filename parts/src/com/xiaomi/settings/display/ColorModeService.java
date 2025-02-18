e0e1bed241f41d67f87ea749acf054b344e56d70
DeanPerformer <142644567+DeandraAir@users.noreply.github.com>
2025-02-18 16:29:22 +0700

    Revert "garnet: parts: Rewrite and cleanup Miui colorservices"
    This reverts commit 3378e01f5a5ea89b6d51bdcf57aaf7b279399353.

--- parts/src/com/xiaomi/settings/display/ColorModeService.java
+++ parts/src/com/xiaomi/settings/display/ColorModeService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023-2024 Paranoid Android
+ * Copyright (C) 2023 Paranoid Android
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -37,36 +37,16 @@ public class ColorModeService extends Service {
         269 /* original */, new DfParams(26, 1, 0),
         268 /* p3 */, new DfParams(26, 2, 0),
         267 /* srgb */, new DfParams(26, 3, 0)
-    private static final int DEFAULT_COLOR_MODE = SystemProperties.getInt("persist.sys.sf.native_mode", 0);
-    private static final DfParams STANDARD_PARAMS = new DfParams(2, 2, 255);
-
+    );
     /* original/p3/srgb */
     private static final int EXPERT_MODE = 26;
-    private static final DfParams EXPERT_PARAMS = new DfParams(EXPERT_MODE, 0, 10);
-
-    /* color mode -> displayfeature (mode, value, cookie) */
-    private static final Map<Integer, DfParams> COLOR_MAP = Map.of(
-        258, new DfParams(0, 2, 255),  // Vivid
-        256, new DfParams(1, 2, 255),  // Saturated
-        257, STANDARD_PARAMS,          // Standard
-        269, new DfParams(26, 1, 0),   // Original
-        268, new DfParams(26, 2, 0),   // P3
-        267, new DfParams(26, 3, 0)    // sRGB
-    );
+    private static final DfParams EXPERT_PARAMS = new DfParams(26, 0, 10);
 
     private final ContentObserver mSettingObserver = new ContentObserver(new Handler()) {
         @Override
         public void onChange(boolean selfChange) {
             Log.e(TAG, "SettingObserver: onChange");
             setCurrentColorMode();
-        }
-    };
-
-    private final BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Log.e(TAG, "onReceive: " + intent.getAction());
-            handleScreenStateChanged(intent);
         }
     };
 
@@ -77,7 +57,6 @@ public class ColorModeService extends Service {
         getContentResolver().registerContentObserver(Settings.System.getUriFor(DISPLAY_COLOR_MODE),
                     false, mSettingObserver, UserHandle.USER_CURRENT);
         setCurrentColorMode();
-        setupService();
     }
 
     @Override
@@ -90,31 +69,12 @@ public class ColorModeService extends Service {
     public void onDestroy() {
         Log.e(TAG, "onDestroy");
         getContentResolver().unregisterContentObserver(mSettingObserver);
-        teardownService();
         super.onDestroy();
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
-    }
-
-    private void setupService() {
-        mAmbientConfig = new AmbientDisplayConfiguration(this);
-        getContentResolver().registerContentObserver(Settings.System.getUriFor(DISPLAY_COLOR_MODE),
-                false, mSettingObserver, UserHandle.USER_CURRENT);
-
-        IntentFilter screenStateFilter = new IntentFilter();
-        screenStateFilter.addAction(Intent.ACTION_SCREEN_ON);
-        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
-        registerReceiver(mScreenStateReceiver, screenStateFilter);
-
-        setCurrentColorMode();
-    }
-
-    private void teardownService() {
-        getContentResolver().unregisterContentObserver(mSettingObserver);
-        unregisterReceiver(mScreenStateReceiver);
     }
 
     private void setCurrentColorMode() {
@@ -130,58 +90,5 @@ public class ColorModeService extends Service {
             DfWrapper.setDisplayFeature(EXPERT_PARAMS);
         }
         DfWrapper.setDisplayFeature(params);
-        if (mIsDozing) {
-            Log.e(TAG, "Skipping color mode change in AOD");
-            return;
-        }
-
-        int colorMode = Settings.System.getIntForUser(getContentResolver(), DISPLAY_COLOR_MODE,
-                DEFAULT_COLOR_MODE, UserHandle.USER_CURRENT);
-
-        DfParams params = COLOR_MAP.getOrDefault(colorMode, STANDARD_PARAMS);
-        Log.e(TAG, "Setting color mode: " + colorMode + ", params=" + params);
-
-        DfWrapper.setDisplayFeature(params.mode == EXPERT_MODE ? EXPERT_PARAMS : params);
-    }
-
-    private void handleScreenStateChanged(Intent intent) {
-        switch (intent.getAction()) {
-            case Intent.ACTION_SCREEN_ON:
-                handleScreenOn();
-                break;
-            case Intent.ACTION_SCREEN_OFF:
-                handleScreenOff();
-                break;
-        }
-    }
-
-    private void handleScreenOn() {
-        if (mIsDozing) {
-            mIsDozing = false;
-            restoreColorModeAfterDoze();
-        }
-    }
-
-    private void restoreColorModeAfterDoze() {
-        mHandler.postDelayed(() -> {
-            Log.e(TAG, "Restoring color mode after AOD");
-            setCurrentColorMode();
-        }, 100);
-    }
-
-    private void handleScreenOff() {
-        if (!mAmbientConfig.alwaysOnEnabled(UserHandle.USER_CURRENT)) {
-            Log.e(TAG, "AOD not enabled");
-            mIsDozing = false;
-            return;
-        }
-        mIsDozing = true;
-        setStandardColorModeForDoze();
-    }
-
-    private void setStandardColorModeForDoze() {
-        mHandler.removeCallbacksAndMessages(null);
-        Log.e(TAG, "Setting standard color mode for AOD");
-        DfWrapper.setDisplayFeature(STANDARD_PARAMS);
     }
 }
